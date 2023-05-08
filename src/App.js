3b7c4d3e27fe30e052adf0bf548b6007209bf0c4
Martin Segnini <68194888+IsTincho@users.noreply.github.com>
2023-05-08 13:44:12 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+import Login from "./components/Login/Login";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
+
+export default App;

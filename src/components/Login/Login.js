14db26e82511be1506c554905074c22eecebadc3
LuchoSantini <lucho_santini7@outlook.es>
2023-05-16 15:28:38 -0300

    Login a terminar

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,81 +1,73 @@
-import React, { useRef, useState } from "react";
-
+import React from "react";
 import "./Login.css";
+// Linea 63 a 66?
+const Login = () => (
+  <section class="h-100 gradient-form">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-lg-5">
+          <div class="card rounded-3 text-black ">
+            <div class="card-body p-md-5 mx-md-4">
+              <div class="text-center">
+                <h4 class="mt-1 mb-5 pb-1">Pagina de Inicio de Sesion</h4>
+              </div>
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [errors, setErrors] = useState([
-    { text: "Email no puede ser vacio", isError: false },
-    { text: "Password no puede ser vacio", isError: false },
-  ]);
+              <form>
+                <p>Por favor ingresa con tu cuenta</p>
 
-  const emailRef = useRef(null);
-  const passwordRef = useRef(null);
+                <div class="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="form2Example11"
+                    class="form-control"
+                    placeholder="Teléfono o Email"
+                  />
+                  <label class="form-label" for="form2Example11">
+                    Nombre de Usuario:
+                  </label>
+                </div>
 
-  const emailChangeHandler = (e) => {
-    setEmail(e.target.value);
-  };
+                <div class="form-outline mb-4">
+                  <input
+                    type="password"
+                    id="form2Example22"
+                    class="form-control"
+                  />
+                  <label class="form-label" for="form2Example22">
+                    Contraseña:
+                  </label>
+                </div>
 
-  // const passwordChangeHandler = (e) => {
-  //   setPassword(e.target.value);
-  // };
+                <div class="text-center pt-1 mb-5 pb-1">
+                  <button
+                    class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                    type="button"
+                  >
+                    Iniciar Sesion
+                  </button>
+                  <a class="text-muted" href="#!">
+                    Olvidé mi contraseña
+                  </a>
+                </div>
 
-  const signInHandler = () => {
-    if (email.length === 0) {
-      emailRef.current.focus();
-      emailRef.current.style.borderColor = "red";
-      emailRef.current.style.outline = "none";
-      const newErrors = [...errors];
-      newErrors[0].isError = true;
-      setErrors(newErrors);
-      return;
-    }
-
-    if (passwordRef.current.value.length === 0) {
-      console.log("here");
-      passwordRef.current.focus();
-      const newErrors = [...errors];
-      newErrors[1].isError = true;
-      setErrors(newErrors);
-      return;
-    }
-    alert(
-      `Su email ${email} y su password ${passwordRef.current.value} han sido registrados`
-    );
-  };
-
-  return (
-    <div className="login-container">
-      <div className="login-box">
-        <h4 className={`${email.length === 0 && "red-text"}`}>
-          ¡Bienvenidos a Book Champions!
-        </h4>
-        <div className="input-container">
-          <input
-            className="input-control"
-            placeholder="Email"
-            type="email"
-            onChange={emailChangeHandler}
-            value={email}
-            ref={emailRef}
-          />
+                <div class="d-flex align-items-center justify-content-center pb-4">
+                  <p class="mb-0 me-2">No tienes una cuenta?</p>
+                  <button type="button" class="btn btn-outline-danger">
+                    Crear cuenta
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+            <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+              <h4 class="mb-4">Brindando la ropa a mejor precio-calidad</h4>
+            </div>
+          </div>
         </div>
-        {errors[0].isError && <p>{errors[0].text}</p>}
-        <div className="input-container">
-          <input
-            className="input-control"
-            placeholder="Password"
-            type="password"
-            ref={passwordRef}
-          />
-        </div>
-        {errors[1].isError && <p>{errors[1].text}</p>}
-        <button onClick={signInHandler} className="signin-button" type="button">
-          Iniciar sesión
-        </button>
       </div>
     </div>
-  );
-};
+  </section>
+);
 
 export default Login;

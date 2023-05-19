8768420e16b8e6916627efb64f53f2ff0e770f90
LuchoSantini <lucho_santini7@outlook.es>
2023-05-18 22:06:46 -0300

    Login hecho, faltan props y path

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,33 +9,34 @@ const Login = () => (
           <div class="card rounded-3 text-black ">
             <div class="card-body p-md-5 mx-md-4">
               <div class="text-center">
-                <h4 class="mt-1 mb-5 pb-1">Pagina de Inicio de Sesion</h4>
+                <h4 class="mt-1 mb-5 pb-1">Iniciar Sesion</h4>
               </div>
 
               <form>
-                <p>Por favor ingresa con tu cuenta</p>
+                <p className="text-center">Por favor ingresa con tu cuenta</p>
 
                 <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example11">
+                    Nombre de Usuario:
+                  </label>
                   <input
                     type="email"
                     id="form2Example11"
                     class="form-control"
                     placeholder="Teléfono o Email"
                   />
-                  <label class="form-label" for="form2Example11">
-                    Nombre de Usuario:
-                  </label>
                 </div>
 
                 <div class="form-outline mb-4">
+                  <label class="form-label" for="form2Example22">
+                    Contraseña:
+                  </label>
                   <input
                     type="password"
                     id="form2Example22"
                     class="form-control"
+                    placeholder="Contraseña"
                   />
-                  <label class="form-label" for="form2Example22">
-                    Contraseña:
-                  </label>
                 </div>
 
                 <div class="text-center pt-1 mb-5 pb-1">
@@ -45,13 +46,15 @@ const Login = () => (
                   >
                     Iniciar Sesion
                   </button>
+                  <br />
                   <a class="text-muted" href="#!">
                     Olvidé mi contraseña
                   </a>
                 </div>
 
-                <div class="d-flex align-items-center justify-content-center pb-4">
-                  <p class="mb-0 me-2">No tienes una cuenta?</p>
+                <div class="d-flex flex-column align-items-center justify-content-center pb-4">
+                  <p class="mb-0 me-2">¿No tienes una cuenta?</p>
+                  <br></br>
                   <button type="button" class="btn btn-outline-danger">
                     Crear cuenta
                   </button>
@@ -59,11 +62,13 @@ const Login = () => (
               </form>
             </div>
           </div>
+          {/* 
           <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
             <div class="text-white px-3 py-4 p-md-5 mx-md-4">
               <h4 class="mb-4">Brindando la ropa a mejor precio-calidad</h4>
             </div>
-          </div>
+          </div> 
+          */}
         </div>
       </div>
     </div>

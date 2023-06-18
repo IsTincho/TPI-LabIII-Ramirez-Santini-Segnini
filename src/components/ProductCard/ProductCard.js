ce9b9dd0c57be1eb2ca81f965ec4a70631eeb18d
Martin Segnini <segninitincho@gmail.com>
2023-06-17 21:41:23 -0300

    Mobile-Styles-added

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -3,11 +3,7 @@ import React from "react";
 import "./ProductCard.css";
 
 const ProductCard = ({ children }) => {
-  return (
-    <div className="book-item-container">
-      {children}
-    </div>
-  );
+  return <div className="book-item-container">{children}</div>;
 };
 
 export default ProductCard;

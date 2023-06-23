bc717fe9d75bd85d85050f0936cc6c50cf982288
Sebastian <sebaramirezx2010@gmail.com>
2023-06-23 13:47:28 -0300

    added RouterProvider for ThemeContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import {
   BrowserRouter as Router,
   Routes,
@@ -7,43 +8,54 @@ import {
 } from "react-router-dom";
 
 import "./App.css";
-
+import { ThemeContext } from "./components/services/theme.context"
 import DashBoard from "./components/DashBoard/DashBoard";
 import Protected from "./components/routes/Protected";
 import NotFound from "./components/routes/NotFound";
 import LoginForm from "./components/Login/LoginForm";
 import AdminProtected from "./components/routes/AdminProtected";
 import AdminPage from "./components/Admin/AdminPage/AdminPage";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
+
 const App = () => {
+  const { theme } = useContext(ThemeContext);
+
+  const router = createBrowserRouter([
+    { path: "/", element: <Navigate to="/login" replace /> },
+    {
+      path: "/login",
+      element: <LoginForm />,
+    },
+    {
+      path: "/home",
+      element: (
+        <Protected>
+          <DashBoard />
+        </Protected>
+      ),
+    },
+    {
+      path: "/admin",
+      element: (
+        <AdminProtected>
+          <AdminPage />
+        </AdminProtected>
+      ),
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route
-          path="/home"
-          element={
-            <Protected>
-              <DashBoard />
-              <ToastContainer />
-            </Protected>
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <AdminProtected>
-              <AdminPage />
-            </AdminProtected>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <div className={`${theme === "dark" && "dark-theme"}`}>
+      <ToastContainer />
+      <RouterProvider router={router} />
+    </div>
   );
 };
 
